e0507c168ac9cf0358fa17257444aa4381d9738e
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-08-27 15:48:38 -0500

    prepro test1

--- controllers/test.js
+++ controllers/test.js
@@ -1,11 +1,11 @@
 module.exports = {
     test : async ()=> {
-        return 'test1 ok'
+        return 'test1 desde pre-pro'
     },
     test2 : async ()=> {
-        return 'test2 ok'
+        return 'test2 ok desde pre-pro'
     },
     test3 : async ()=> {
-        return 'test3 ok'
+        return 'test3 ok desde pre-pro'
     }
 }
