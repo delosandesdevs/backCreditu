febca96d7623e87437b5dd9c46a075ed9828099b
Florencia Taburelli <93846302+FlorenciaTaburelli@users.noreply.github.com>
2022-08-30 17:32:34 -0300

    add model player, add routes player, add img avatares

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {app} = require('./app')
 const { sequelize } = require('./db/db')
 
 let port = 8080
-
+console.log(sequelize.models)
 app.listen(port, () => {
     console.log('Server run on Port =>  ' + port)
     sequelize.sync({ alter: true })
---
+++ models/Player.js
@@ -0,0 +1,41 @@
+const {sequelize, DataTypes, Op } = require ('../db/db.js')
+
+
+const Player = sequelize.define('player', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    nickname : {
+        type : DataTypes.STRING,
+        allowNul : false
+    },
+    score: {
+        type : DataTypes.INTEGER,
+        allowNul : false
+    },
+    status: {
+        type : DataTypes.STRING,
+        set(value) {
+            if (value >= 0 && value < 5000){
+                this.setDataValue('status','bronce')
+            }else if (value >= 5000 && value < 9000){
+                this.setDataValue('status','plata')
+            }else if (value >= 9000 && value <= 10000){
+                this.setDataValue('status','oro')
+            }
+          }
+    }, 
+    avatar: {
+        type : DataTypes.STRING,
+        allowNull : false
+    }
+    },
+    {
+        freezeTableName : true 
+    })
+
+module.exports = Player
