352f43dac5f871d1cc8cc82d246b6b890ddabb7a
Florencia Taburelli <93846302+FlorenciaTaburelli@users.noreply.github.com>
2022-08-31 20:55:26 -0300

    fix env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const cors = require("cors")
 const { sequelize } = require('./db/db')
 const app = express()
 const test = require('./routes/test')
-const dotenv = require('dotenv')
 
-dotenv.config()
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
