6d308f1f384d84c51cc0748409455004a26b97fc
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-09-09 20:10:14 -0500

    test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const {app} = require('./app')
 const { sequelize } = require('./db/db')
 
 let port = 8080
+const {PORT} = process.env
+console.log(process.env)
 
-app.listen(port, () => {
-  console.log('Server run on Port =>  ' + port)
+app.listen(PORT, () => {
+  console.log('Server run on Port =>  ' + PORT)
   sequelize.sync({ alter: true })
 })
 
