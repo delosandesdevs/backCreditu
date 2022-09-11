4c1f63c727b3baad8570be9e6ff74b7a97d42439
Florencia Taburelli <93846302+FlorenciaTaburelli@users.noreply.github.com>
2022-09-11 14:32:05 -0300

    test malo

--- test/player.spec.js
+++ test/player.spec.js
@@ -337,7 +337,7 @@ describe('CRUD Players', ()=>{
       await User.create({name: 'florencia', email: 'florencia@gmail.com', role: 'admin'})
       const newPlayer = await Player.create({nickname: 'florGesell', avatar: 'image.png', score: 56, user_id: 1})
       const response = await api.put(`/players/${newPlayer.dataValues.id}`).send({score: 4000})
-      expect(response.statusCode).toBe(400) 
+      expect(response.statusCode).toBe(700) 
       expect(response.body.message).toEqual('un user_id es requerido')
     })
     // errro 400 si no se pasa user_id
